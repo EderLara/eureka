b0a4f7b8c9edfa5005583f51984f6020f50388b9
Eder Lara Trujillo <ederlatru@gmail.com>
2019-12-08 17:51:10 -0500

    Actualización de la vista, creación de formulario de registro

--- view/src/app/componentes/registro/registro.component.ts
+++ view/src/app/componentes/registro/registro.component.ts
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  public rol: boolean;
+  public registroimg: string;
+  public description: string;
+  constructor() {
+    this.rol = false;
+    this.registroimg = 'assets/media/img/dashboard.jpg';
+    this.description = 'Eureka, es una Aplicación de prueba, usada para el semillero de investigación Sintaxis del SENA Regional Antioquia del centro de la manufactura avanzada CTMA';
+   }
 
   ngOnInit() {
   }
